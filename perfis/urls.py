90bd8898f25aa6a36bbb450d719ce7a35b56312a
Gabriel Machado <g_machado1@hotmail.com>
2018-07-12 17:43:35 -0300

    Implementado pagina de detalhes de perfil

--- perfis/urls.py
+++ perfis/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from perfis import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('perfis/<int:perfil_id>', views.exibir, name='exibir')
 ]
--- perfis/views.py
+++ perfis/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from perfis.models import Perfil
 
 def index(request):
     return render(request, 'index.html')
+
+def exibir(request, perfil_id):
+    perfil = Perfil()
+
+    if (perfil_id == 1):
+        perfil = Perfil('Flavio Almeida', 'flavio@flavio.com.br', '777777', 'Caelum')
+    if (perfil_id == 2):
+        perfil = Perfil('Romulo Henrique', 'romulo@romulo.com.br', '888888', 'Caelum')
+
+    return render(request, 'perfil.html', { 'perfil': perfil })
